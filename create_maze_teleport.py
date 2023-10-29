e47aaa78e0f1b7d6721450356b50627c29001455
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-10-29 16:38:23 +0700

    test bao đỉnh

--- create_maze_teleport.py
+++ create_maze_teleport.py
@@ -1,41 +1,66 @@
+#  Đi xuống và vào Tele
 with open('input/advance/maze_teleport1.txt', 'w') as outfile:
-    outfile.write('1\n')
-    outfile.write('5 21 13 1\n')
-    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
-    outfile.write('x S                        x  x\n')
-    outfile.write('x xxxxxxxxxxxxxxxxxxxxxxxxxx  x\n')
-    outfile.write('x x                           x\n')
-    outfile.write('x x xxxxxxxxxxxxxxxxxxxxxxxx  x\n')
-    outfile.write('x x x                Tx    x  x\n')
-    outfile.write('x x x xxxxxxxxxxxxxxxxx x  x  x\n')
-    outfile.write('x x x                   x  x  x\n')
-    outfile.write('x x xxxxxxxxxxxxxxxxxxxxx  x  x\n')
-    outfile.write('x x                        x  x\n')
-    outfile.write('x xxxxxxxxxxxxxxxxxxxxxxxxxx  x\n')
-    outfile.write('x                             x\n')
-    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
-    outfile.write('xT                             \n')
-    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-
-with open('input/advance/maze_teleport2.txt', 'w') as outfile:
     outfile.write('2\n')
-    outfile.write('1 6 6 23\n')
+    outfile.write('1 5 7 23\n')
     outfile.write('9 3 3 30\n')
     outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
-    outfile.write('x S                 T         x\n')
+    outfile.write('x S  T                        x\n')
     outfile.write('x xxxxxxxxxxxxxxxxxxxxxxxxxxx x\n')
-    outfile.write('x x                           x\n')
-    outfile.write('x x xxxxxxxxxxxxxxxxxxxxxxxxx x\n')
+    outfile.write('x x                           T\n')
+    outfile.write('x x xxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
     outfile.write('x x x                      x  x\n')
-    outfile.write('x x x xxxxxxxxxxxxxxxxxxx  x  T\n')
-    outfile.write('x x x                  Tx  x   \n')
+    outfile.write('x x x xxxxxxxxxxxxxxxxxxx  x  x\n')
+    outfile.write('x x x                  Tx  x  x\n')
     outfile.write('x x xxxxxxxxxxxxxxxxxxxxx  x  x\n')
     outfile.write('x xT                       x  x\n')
-    outfile.write('x xxxxxxxxxxxx xxxxxxxxxxxxx  x\n')
+    outfile.write('x xxx xxxxxxxxxxxxxxxxxxxxxx  x\n')
     outfile.write('x                             x\n')
     outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
 
+#  Xuống -> Telee -> Lên + qua trái -> Tele => Đi về thằng đích
+with open('input/advance/maze_teleport2.txt', 'w') as outfile:
+    outfile.write('3\n')
+    outfile.write('3 25 12 4\n')
+    outfile.write('10 10 7 29\n')
+    outfile.write('6 1 5 19\n')
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
+    outfile.write('x S                          x\n');
+    outfile.write('x  xxxxxxxxxxxxxxxxxxxxxxxx  x\n');
+    outfile.write('x  x   T                  x  x\n');
+    outfile.write('x  x xxxxxxxx xxxxxxxxxxxxx  x\n');
+    outfile.write('x  x             T           x\n');
+    outfile.write('xT xxxxxxxxxxxxxxxxxxxxxxxxx x\n');
+    outfile.write('x  x   x         x          xT\n');
+    outfile.write('x  x x x x      xT  x       xx\n');
+    outfile.write('x  x x x x       x           x\n');
+    outfile.write('x  x x   x                   x\n');
+    outfile.write('x  x xxxxxxxxxxxxxxxxxxxxxxxxx\n');
+    outfile.write('x  xT                        x\n');
+    outfile.write('x  xxxx xxxxxxxxxxxxxxxxxxx  x\n');
+    outfile.write('x                            x\n');
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
+
+# Đi xuống -> Teleport -> Qua trái -> Teleport -> Teleport
 with open('input/advance/maze_teleport3.txt', 'w') as outfile:
+    outfile.write('2\n')
+    outfile.write('4 1 7 23\n')
+    outfile.write('9 3 6 30\n')
+    outfile.write('7 18 9 3\n')
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+    outfile.write('x S                           x\n')
+    outfile.write('x xxxxxxxxxxxxxxxxxxxxxxxxxxx x\n')
+    outfile.write('x x                           x\n')
+    outfile.write('xTx xxxxxxxxxxxxxxxxxxxxxxxxx x\n')
+    outfile.write('x x x                      x  x\n')
+    outfile.write('x x x xxxxxxxxxxxxxxxxxxx  x  T\n')
+    outfile.write('x x x             T    Tx  x   \n')
+    outfile.write('x x xxxxxxxxxxxxxxxxxxxxx  x  x\n')
+    outfile.write('x xT                       x  x\n')
+    outfile.write('x xxxxxx xxxxxxxxxxxxxxxxxxx  x\n')
+    outfile.write('x                             x\n')
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
+
+with open('input/advance/maze_teleport4.txt', 'w') as outfile:
     outfile.write('3\n')
     outfile.write('3 25 12 4\n')
     outfile.write('10 10 7 29\n')
@@ -46,11 +71,11 @@ with open('input/advance/maze_teleport3.txt', 'w') as outfile:
     outfile.write('x  x                     Tx  x\n');
     outfile.write('x  x xxxxxxxxxxxxxxxxxxxxxx  x\n');
     outfile.write('x  x               T         x\n');
-    outfile.write('xT xxxxxxxxxxxxxxxxxxxxxxxxx x\n');
-    outfile.write('x  x   x                    xT\n');
+    outfile.write('xT xxxxxxxxxxxxxxxxxxxxxxxx xx\n');
+    outfile.write('x  x   x                   x T\n');
     outfile.write('x  x x x x                  xx\n');
-    outfile.write('x  x x x xxxxxxxxxxxxxxxxxx  x\n');
-    outfile.write('x  x x   xT                  x\n');
+    outfile.write('x  x x x x                   x\n');
+    outfile.write('x  x x   x             T     x\n');
     outfile.write('x  x xxxxxxxxxxxxxxxxxxxxxxxxx\n');
     outfile.write('x  xT                        x\n');
     outfile.write('x  xxxxxxxxxxxxxxxxxxxxxxxx  x\n');
