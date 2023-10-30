061c02b7a22b2d62ef9c98f4c2c1bf266ce87b96
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-10-30 15:17:27 +0700

    Change test3 destination

--- create_map_bonus.py
+++ create_map_bonus.py
@@ -1,4 +1,16 @@
-with open('input/leveL_2/maze_destination1.txt', 'w') as outfile:
+with open('input/leveL_2/maze_destination2.txt', 'w') as outfile:
+    outfile.write("2\n")
+    outfile.write('1 1 -5')
+    outfile.write('1 8 -100')
+    outfile.write('xxxxxxxxxx\n')
+    outfile.write('x+x   x +x\n')
+    outfile.write('x x x x  x\n')
+    outfile.write('x x x x  x\n')
+    outfile.write('xSx x x  x\n')
+    outfile.write('    x    x\n')
+    outfile.write('xxxxxxxxxx\n')
+
+with open('input/leveL_2/maze_destination2.txt', 'w') as outfile:
     outfile.write('5\n')
     outfile.write('1 28 -5\n')
     outfile.write('3 3 -50\n')
@@ -19,21 +31,32 @@ with open('input/leveL_2/maze_destination1.txt', 'w') as outfile:
     outfile.write('x+                             \n')
     outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n')
 
-# with open('input/leveL_3/maze_destination2.txt', 'w') as outfile:
-#     outfile.write('10\n')
-#     outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
-#     outfile.write('x S           +              x\n');
-#     outfile.write('x   xxxxxxxxxxxxxxxxxxxxxxxx x\n');
-#     outfile.write('x         x + x    x         x\n');
-#     outfile.write('x       + x   x     x        x\n');
-#     outfile.write('x     x              x  +    x\n');
-#     outfile.write('x   xxxxxxxxxxx  +    x      x\n');
-#     outfile.write('x             x      x x     x\n');
-#     outfile.write('xxxxxxxxxxxxx x     x + x    x\n');
-#     outfile.write('x            +x          x   x\n');
-#     outfile.write('x xxxxxxxxxxxxx     x x x x  x\n');
-#     outfile.write('              x    +         x\n');
-#     outfile.write('x             x     xxxx     x\n');
-#     outfile.write('xxxxxxxxxxx   x              x\n');
-#     outfile.write('x +                         +x\n');
-#     outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
+with open('input/leveL_3/maze_destination3.txt', 'w') as outfile:
+    outfile.write('10\n')
+    outfile.write('1 14 2\n')
+    outfile.write('3 12 -15\n')
+    outfile.write('4 8 -10\n')
+    outfile.write('5 24 -5\n')
+    outfile.write('6 17 -7\n')
+    outfile.write('8 22 -10\n')
+    outfile.write('9 13 -100\n')
+    outfile.write('11 23 -2\n')
+    outfile.write('14 2 -10\n')
+    outfile.write('14 28 -200\n')
+
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
+    outfile.write('x S           +               \n');
+    outfile.write('x   xxxxxxxxxxxxxxxxxxxxxxxx x\n');
+    outfile.write('x         x + x    x         x\n');
+    outfile.write('x       + x   x     x        x\n');
+    outfile.write('x     x              x  +    x\n');
+    outfile.write('x   xxxxxxxxxxx  +    x      x\n');
+    outfile.write('x             x      x x     x\n');
+    outfile.write('xxxxxxxxxxxxx x     x + x    x\n');
+    outfile.write('x            +x              x\n');
+    outfile.write('x xxxxxxxxxxxxx     x x xxx  x\n');
+    outfile.write('x             x        + x   x\n');
+    outfile.write('x             x     xxxxx    x\n');
+    outfile.write('xxxxxxxxxxx   x              x\n');
+    outfile.write('x +                         +x\n');
+    outfile.write('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n');
